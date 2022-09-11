99bf2d84d58572298509064a7a94925323b883da
ariefbraja <marief.braja@gmail.com>
2022-09-11 21:30:47 +0700

    Tugas 2 ver 1

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog': data_barang_katalog,
+        'nama': 'Muhammad Arief Braja Putra',
+        'NPM': '2106702352',
+    }
+    return render(request, "katalog.html", context)
