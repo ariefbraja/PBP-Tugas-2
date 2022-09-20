b27d80ed35ce59c8ba4d687e7b5a32702e6c4252
ariefbraja <marief.braja@gmail.com>
2022-09-20 22:15:06 +0700

    Tugas 3

---
+++ mywatchlist/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class MyWatchList(models.Model):
+    watched = models.TextField()
+    title = models.TextField()
+    rating = models.FloatField()
+    release_date = models.TextField()
+    review = models.TextField()
+
